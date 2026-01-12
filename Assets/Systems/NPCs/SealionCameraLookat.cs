d69b4e5371ce777d6fa7d1e1c448fb33f5056397
RuinedVLR <rzhurakhov@gmail.com>
2026-01-12 12:29:16 -0400

    sealion

---
+++ Assets/Systems/NPCs/SealionCameraLookat.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SealionCameraLookat : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+
+    }
+}
